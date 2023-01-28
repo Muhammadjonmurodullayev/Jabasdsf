d786816929bd8259e6d269e0e5c7be6eea41bc45
Muhammadjonmurodullayev <Muhammadjonmurodullayev2005@gmail.com>
2023-01-28 17:02:05 +0500

    Dara-3

--- Dars-3/index.js
+++ Dars-3/index.js
@@ -3,9 +3,55 @@
 // let  b="1"
 // console.log( typeof a);
 // typof : deb yossak manashu malumotlarni tyupni chiqarib beradi 
- 
+
 //////  +("a") ---- qo'yilsa u aval string edi ketin typni numberga o'zgartirdi
 // let  a=1
 // let b="3s"//misol
 //////    agar stringni ichida har f bo'lib qoldigan bo'sa u {"NAN"} degan qiymat chiqarib beradi
 ///// yani {a="1s"} manabu sitring raqam sifatiida ifodalab bo'lmaydi degani
+// let a=2
+// let b="3"
+// console.log(a+ Number(bu  yerga to'g'ridan to'g'ri raqam ham yossak bo'ladi));
+//////  yani : Agar consol.logni icida Number qiymat yozadigan bo'lsak numberni ichidagi qiymatni songa o'zgartirib beradi
+// Kars : sonlarni float sonlar deyiladi
+// console.log(a+ Number(b));
+// console.log(a+ Number.parseInt(b));
+// console.log(a+ parseInt(b));
+// Number-bu faqat sitring bo'lgan sonlarni numberga o'zgartirib berradi
+// parseInt - faqat butun qismini oladi 
+// parseFloat-bu kasr qismini  ham olib beradi 
+//////   Boolean
+// true- ha degani.
+// false- yo'q degani.
+// let a = 1
+// let b = 2
+// console.log(a + b * a);
+// let a = 3
+// let b = 2
+// console.log(a % b);
+// console.log(b % a)
+////////////// '%' -- qoldiqni chiqarish uchun ishlatiladi
+// console.log(2 * 2);  //      javobi: 8
+// console.log(2 * 2 * 2); //   javobi: 8
+// console.log(2 * 2 * 2 * 2);//javobi: 16
+// console.log(2**20); // javobi 1048576 bu 2ta {**} kvadirat maosini  bildiradi va kvadiratga ko'tarib beradi
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
