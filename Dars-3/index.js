85bc5cdd4128a209417c3c0063144e78706ffd37
Muhammadjonmurodullayev <Muhammadjonmurodullayev2005@gmail.com>
2023-01-28 16:00:03 +0500

    Dars-3

--- Dars-3/index.js
+++ Dars-3/index.js
@@ -1,1 +1,11 @@
-console.log("salom");
+// console.log("salom");
+// let  a=1
+// let  b="1"
+// console.log( typeof a);
+// typof : deb yossak manashu malumotlarni tyupni chiqarib beradi 
+ 
+//////  +("a") ---- qo'yilsa u aval string edi ketin typni numberga o'zgartirdi
+// let  a=1
+// let b="3s"//misol
+//////    agar stringni ichida har f bo'lib qoldigan bo'sa u {"NAN"} degan qiymat chiqarib beradi
+///// yani {a="1s"} manabu sitring raqam sifatiida ifodalab bo'lmaydi degani
