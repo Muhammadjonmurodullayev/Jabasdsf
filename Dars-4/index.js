3653ab17ce4d7ce117cd22b61aa48f2e41b24b61
Muhammadjonmurodullayev <Muhammadjonmurodullayev2005@gmail.com>
2023-01-31 14:51:00 +0500

    Dars-4

--- Dars-4/index.js
+++ Dars-4/index.js
@@ -8,15 +8,57 @@
 // else {
 //     console.log("Bunaqa ob havo mavjud emas ");
 // }
-let box = 2
-if (0 > box) {
-    console.log("Havo juda sovuq");
-} if else (0 < box){
-    console.log("Havo sovuq");
-} if else (10 > box){
-    console.log(Havo issiq);
-} if else (10 < box){
-    console.log("Havo juda iisiq");
-}else {
-    console.log("Bunday ob havo mavjud emas");
+// let box = 10;
+
+// if (0 > box) { console.log("Havo juda sovuq"); }
+// else if (0 < box) { console.log("Havo sovuq"); }
+// else if (10 > box) { console.log("Havo issiq"); }
+// else if (10 < box) { console.log("Havo juda isiq"); }
+// else { console.log("Bunday ob havo mavjud emas"); }
+
+// let box = 20;
+
+// if (0 > box) { console.log("Havo juda sovuq"); }
+// else if (0 < box && 10 > box) { console.log("Havo sovuq"); }
+// else if (10 < box && 20 > box) { console.log("Havo juda isiq"); }
+// else { console.log("Bunday ob havo mavjud emas"); }
+//  '?' -------to'rniriy opireytit deyiladi buni faqat ikkita holatni  taqqosalash uchun ishlaytgan afzal. Faqat ikatabhoolatni taqqolash ucun ishlatilishi kk 
+// let lamp = fa;
+// if (lamp) {
+//     console.log("Chiroq on");
+// } else {
+//     console.log("Chiroq off");
+// }
+// let lamp = 5;
+// lamp > 0 && lamp < 10 ? console.log("Chiroq on") :
+// lamp < 10 && lamp > 20 ? console.log("Chiroq off");
+let login = 'webbrain';
+let pw = 12345;
+// null yoki undefined kelmasa boldi '?' turner opireytet ishliydi
+if (login && pw){
+    console.log("Welcom to Fezbook");
+}else{
+    console.log("login yoki parol xato");
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
