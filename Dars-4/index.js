7d8f5783398b913f7c7926f60d345bb94e703590
Muhammadjonmurodullayev <Muhammadjonmurodullayev2005@gmail.com>
2023-01-31 13:30:04 +0500

    Dars-4

--- Dars-4/index.js
+++ Dars-4/index.js
@@ -8,3 +8,15 @@
 // else {
 //     console.log("Bunaqa ob havo mavjud emas ");
 // }
+let box = 2
+if (0 > box) {
+    console.log("Havo juda sovuq");
+} if else (0 < box){
+    console.log("Havo sovuq");
+} if else (10 > box){
+    console.log(Havo issiq);
+} if else (10 < box){
+    console.log("Havo juda iisiq");
+}else {
+    console.log("Bunday ob havo mavjud emas");
+}
