dc9cb022c0bdcb15b0fe89c4cd02999be8339d74
Muhammadjonmurodullayev <Muhammadjonmurodullayev2005@gmail.com>
2023-02-02 19:46:24 +0500

    Dars-4

--- Dars-5/Index.js
+++ Dars-5/Index.js
@@ -1,22 +1,38 @@
+// let chiroq = "chiroq on";
+//  chiroq = "chiroq off"
+// switch (chiroq) {
+//         case "chiroq on": 
+//         console.log("chiqor on");
+//         break;
+
+//         case "chiroq off": 
+//         console.log("chiqor off");
+//         break;
+// }
+// let i = 1
+// for (i; i < 10; i++) {
+//         console.log(i);
+// }
 // let Chiroq = "Chiroq on"
 //     Chiroq = "Chiroq off"
 // switch (Chiroq) {
 //     case "Chiroq on":
 //         console.log("Chiroq yoniq");
 //         break;
-//     case "Chiroq off": 
+//     case "Chiroq off":
 //         console.log("Chiroq o'chiq");
 // break;
 //     }
         //   123
 // let str = "Bir yuz yigirma uch"
 // let  = "Asaa"
-     chiroq = "Chiroq on"
-    Chiroq = "Chiroq off"
-switch (Chiroq) {
-    case "Chiroq on":
-        console.log("Chiroq yoniq");
-        break;
-    case "Chiroq off": 
-        console.log("Chiroq o'chiq");
-break;}
+//   let    chiroq = "Chiroq on"
+//     Chiroq = "Chiroq off"
+// switch (Chiroq) {
+//     case "Chiroq on":
+//         console.log("Chiroq yoniq");
+//         break;}
+// let str = "Bir yuz yigirma uch";
+// console.log(str[0,]);
+//       for loops 
+
