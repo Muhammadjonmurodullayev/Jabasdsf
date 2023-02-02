85148e2a354bd8848a09e1c33774b69da51dca29
Muhammadjonmurodullayev <Muhammadjonmurodullayev2005@gmail.com>
2023-02-02 20:48:03 +0500

    Dars-4

--- Dars-5/Index.js
+++ Dars-5/Index.js
@@ -35,7 +35,33 @@
 // let str = "Bir yuz yigirma uch";
 // console.log(str[0,]);
 //       for loops 
-let i = 1
-for (i; i < 10; i=i+2) {
-        console.log(i);
-}
+// let i = 1
+// for (i; i < 10; i=i+2) {
+//         console.log(i);
+// // }
+// let i = 1;
+// for (i; i <= 100; ++i)
+//         if (i % 2 === 0) {
+//                 console.log(i, "Juft son");
+//         } else 
+//                 {
+//                         console.log(i, "toq son");
+                
+        // console.log((i % 2) /2);
+        // console.log((i % 2) +2);
+        //  console.log((i /2) %2);
+        // console.log(i % 22 === 0);
+                        // Homework
+  switch(browser){
+        case 'Edge' :
+   alert("You ve got the edge!");
+   break;
+   alert('Okay we support these browsers too!');
+    break;
+    alert('Okay we support these browsers too!');
+    default:
+        alert('We hope that this page looks ok!');
+
+  }                      
+
+
