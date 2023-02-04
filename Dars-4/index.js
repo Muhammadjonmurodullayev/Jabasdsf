db8bd4e1bd6c25fd1a77a9e3e5a6c83ffc56e8cc
Muhammadjonmurodullayev <Muhammadjonmurodullayev2005@gmail.com>
2023-02-04 10:14:59 +0500

    Dars-4

--- Dars-4/index.js
+++ Dars-4/index.js
@@ -1,60 +1,106 @@
-// let lamp = -1;
-// if else (0 > lamp) {
-//     console.log("Havo juda sovuq");
+// let chiroq = "chiroq on";
+//  chiroq = "chiroq off"
+// switch (chiroq) {
+//         case "chiroq on": 
+//         console.log("chiqor on");
+//         break;
+
+//         case "chiroq off": 
+//         console.log("chiqor off");
+//         break;
 // }
-// if else(0 < lamp) {
-//     console.log("Havo juda issiq");
+// let i = 1
+// for (i; i < 10; i++) {
+//         console.log(i);
 // }
-// else {
-//     console.log("Bunaqa ob havo mavjud emas ");
+// let Chiroq = "Chiroq on"
+//     Chiroq = "Chiroq off"
+// switch (Chiroq) {
+//     case "Chiroq on":
+//         console.log("Chiroq yoniq");
+//         break;
+//     case "Chiroq off":
+//         console.log("Chiroq o'chiq");
+// break;
+//     }
+//   123
+// let str = "Bir yuz yigirma uch"
+// let  = "Asaa"
+//   let    chiroq = "Chiroq on"
+//     Chiroq = "Chiroq off"
+// switch (Chiroq) {
+//     case "Chiroq on":
+//         console.log("Chiroq yoniq");
+//         break;}
+// let str = "Bir yuz yigirma uch";
+// console.log(str[0,]);
+//       for loops 
+// let i = 1
+// for (i; i < 10; i=i+2) {
+//         console.log(i);
+// // }
+// let i = 1;
+// for (i; i <= 100; ++i)
+//         if (i % 2 === 0) {
+//                 console.log(i, "Juft son");
+//         } else 
+//                 {
+//                         console.log(i, "toq son");
+
+// console.log((i % 2) /2);
+// console.log((i % 2) +2);
+//  console.log((i /2) %2);
+// console.log(i % 22 === 0);
+//                                         Homework
+// switch(browser){
+//       case 'Edge' :
+//  alert("You ve got the edge!");
+//  break;}
+//    alert('Okay we support these browsers too!');
+//     break;
+//     alert('Okay we support these browsers too!');
+//     default:
+//         alert('We hope that this page looks ok!');
+
+//   }                    
+//   case 'Chrome' :
+//         case 'Firefox' :
+//                 case 'Safari' :
+//                         case 'opera' :
+// 2)
+// let a = +prompt('a?', 'b')
+// let b = 1
+// if(aa==0){
+//         alert(0);
+//         }
+// if(aa==1){
+//  alert(1);
 // }
-// let box = 10;
-
-// if (0 > box) { console.log("Havo juda sovuq"); }
-// else if (0 < box) { console.log("Havo sovuq"); }
-// else if (10 > box) { console.log("Havo issiq"); }
-// else if (10 < box) { console.log("Havo juda isiq"); }
-// else { console.log("Bunday ob havo mavjud emas"); }
-
-// let box = 20;
-
-// if (0 > box) { console.log("Havo juda sovuq"); }
-// else if (0 < box && 10 > box) { console.log("Havo sovuq"); }
-// else if (10 < box && 20 > box) { console.log("Havo juda isiq"); }
-// else { console.log("Bunday ob havo mavjud emas"); }
-//  '?' -------to'rniriy opireytit deyiladi buni faqat ikkita holatni  taqqosalash uchun ishlaytgan afzal. Faqat ikatabhoolatni taqqolash ucun ishlatilishi kk 
-// let lamp = fa;
-// if (lamp) {
-//     console.log("Chiroq on");
-// } else {
-//     console.log("Chiroq off");
+//       if(a==2||a==3a)  {
+//         alert('2.3');
+//       }
+// let i=3;
+// while(i){
+//   console.log(i--);
+// } javobi:3,2,1
+// let i=0;
+// while((++i<5))console.log(i);javobi:1,2,3,4
+// let i=0;
+// while(i++<5)console.log(i);javobi:1,2,3,4,5
+// for (let i=0; i<3; i++){
+//   console.log(`number ${i}!`);
+// }javobi:number 0! number 1! number 2!
+        // juft ///toq
+// var i = 1
+// for (i; i <= 100; ++i) {
+//   if(i % 2 === 0){
+//     console.log(i,"Juft son");
+//   }else{
+//     console.log(i,"Toq son");
+//   }
 // }
-// let lamp = 5;
-// lamp > 0 && lamp < 10 ? console.log("Chiroq on") :
-// lamp < 10 && lamp > 20 ? console.log("Chiroq off");
-// let login = 'webbrain';
-// let pw = 12345;
-// // null yoki undefined kelmasa boldi '?' turner opireytet ishlaydi
-// if (login && pw){
-//     console.log("Welcom to Fezbook");
-// }else{
-//     console.log("login yoki parol xato");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// let t =1
+// for(t,t)
 
 
 
