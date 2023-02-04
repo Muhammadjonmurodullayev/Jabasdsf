449163402b9c3bcffdb53725eedc5f3c0b861ddc
Muhammadjonmurodullayev <Muhammadjonmurodullayev2005@gmail.com>
2023-02-04 12:01:22 +0500

    Dars-4

--- Dars-5/Index.js
+++ Dars-5/Index.js
@@ -1,106 +1,13 @@
-// let chiroq = "chiroq on";
-//  chiroq = "chiroq off"
-// switch (chiroq) {
-//         case "chiroq on": 
-//         console.log("chiqor on");
-//         break;
+// function get(){
+//     console.log('Hello js');
+// }
+// get()
+let name ="webbrain"
+console.log(name);
 
-//         case "chiroq off": 
-//         console.log("chiqor off");
-//         break;
-// }
-// let i = 1
-// for (i; i < 10; i++) {
-//         console.log(i);
-// }
-// let Chiroq = "Chiroq on"
-//     Chiroq = "Chiroq off"
-// switch (Chiroq) {
-//     case "Chiroq on":
-//         console.log("Chiroq yoniq");
-//         break;
-//     case "Chiroq off":
-//         console.log("Chiroq o'chiq");
-// break;
-//     }
-//   123
-// let str = "Bir yuz yigirma uch"
-// let  = "Asaa"
-//   let    chiroq = "Chiroq on"
-//     Chiroq = "Chiroq off"
-// switch (Chiroq) {
-//     case "Chiroq on":
-//         console.log("Chiroq yoniq");
-//         break;}
-// let str = "Bir yuz yigirma uch";
-// console.log(str[0,]);
-//       for loops 
-// let i = 1
-// for (i; i < 10; i=i+2) {
-//         console.log(i);
-// // }
-// let i = 1;
-// for (i; i <= 100; ++i)
-//         if (i % 2 === 0) {
-//                 console.log(i, "Juft son");
-//         } else 
-//                 {
-//                         console.log(i, "toq son");
 
-// console.log((i % 2) /2);
-// console.log((i % 2) +2);
-//  console.log((i /2) %2);
-// console.log(i % 22 === 0);
-//                                         Homework
-// switch(browser){
-//       case 'Edge' :
-//  alert("You ve got the edge!");
-//  break;}
-//    alert('Okay we support these browsers too!');
-//     break;
-//     alert('Okay we support these browsers too!');
-//     default:
-//         alert('We hope that this page looks ok!');
 
-//   }                    
-//   case 'Chrome' :
-//         case 'Firefox' :
-//                 case 'Safari' :
-//                         case 'opera' :
-// 2)
-// let a = +prompt('a?', 'b')
-// let b = 1
-// if(aa==0){
-//         alert(0);
-//         }
-// if(aa==1){
-//  alert(1);
-// }
-//       if(a==2||a==3a)  {
-//         alert('2.3');
-//       }
-// let i=3;
-// while(i){
-//   console.log(i--);
-// } javobi:3,2,1
-// let i=0;
-// while((++i<5))console.log(i);javobi:1,2,3,4
-// let i=0;
-// while(i++<5)console.log(i);javobi:1,2,3,4,5
-// for (let i=0; i<3; i++){
-//   console.log(`number ${i}!`);
-// }javobi:number 0! number 1! number 2!
-        // juft ///toq
-// var i = 1
-// for (i; i <= 100; ++i) {
-//   if(i % 2 === 0){
-//     console.log(i,"Juft son");
-//   }else{
-//     console.log(i,"Toq son");
-//   }
-// }
-// let t =1
-// for(t,t)
+
 
 
 
