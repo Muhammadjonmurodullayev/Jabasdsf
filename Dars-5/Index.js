1580de48a7b4bb6ca4c4e764c1177c7c0668c929
Muhammadjonmurodullayev <Muhammadjonmurodullayev2005@gmail.com>
2023-02-04 15:44:49 +0500

    Dars-5

--- Dars-5/Index.js
+++ Dars-5/Index.js
@@ -1,5 +1,6 @@
 // function get(){
 //     console.log('Hello js');
+//     alert('Hello js');
 // }
 // get()
 // let name ="webbrain"
@@ -18,19 +19,52 @@
 //     console.log(name);
 //  }
 // get("Muhammadjon")
-let total = 208;
-function get(grade) {
-    let res = (grade * 100) / 208;
-    console.log(res);
-}
-get()
-get(50);
-get(104);
+// let total = 208;
+// function get(grade) {
+//     let res = (grade * 100) / 208;
+//     console.log(res);
+// }
+// get()
+// get(50);
+// get(104);
+// function getname(name,surname,){
+//     console.log(name,surname);
+// }
+// getname("Eshmat","Toshmatov")
+// function get(grade) {
+//     let res = (grade * 100) / 208;
+//     return res;
+// }
+// console.log(get(105) > 50 ? "Passed" : "failed");
+// function get(){} buni xoxlagan joyda chaqira olamiz
+// const get = function(){} -buni faqat o'zidan keyin chaqirish kerak
+// const arrFun=()=>{}
+// Homework
+// for(let i=0;i<3;i++){
+//     console.log(`number ${i}!`);
 
-
-
-
-
-
+// }
+// javobi:
+// number 0!
+// number 1!
+// number 2!
+// let age =19;
+// function checkAge(age){
+//     console.log(age);
+//     if(age>18){
+//         return true;
+//     }else{
+//         console.log("Siz akaunt ocholmaysiz");
+//         return confirm('Did parents allow you?')
+//     }
+// }
+// checkAge(age)
+// console.log(Min(2,5));
+// Pow(3, 2) = 3 * 3=9
+// Pow(3, 3) = 3 * 3 * 3=27
+// Pow(1,100) = 1*1...*1=1
+// console.log(3**2);=9
+// console.log(3**3);=27
+// console.log(1*100);=100
 
 
