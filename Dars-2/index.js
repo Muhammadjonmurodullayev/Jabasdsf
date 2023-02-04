29d1a1348159d664f990196abd4a8b50583910f5
Muhammadjonmurodullayev <Muhammadjonmurodullayev2005@gmail.com>
2023-02-04 10:10:24 +0500

    Dars-2

---
+++ Dars-2/index.js
@@ -0,0 +1,160 @@
+// console.log("salom");
+// let  a=1
+// let  b="1"
+// console.log( typeof a);
+// typof : deb yossak manashu malumotlarni tyupni chiqarib beradi 
+
+//////  +("a") ---- qo'yilsa u aval string edi ketin typni numberga o'zgartirdi
+// let  a=1
+// let b="3s"//misol
+//////    agar stringni ichida har f bo'lib qoldigan bo'sa u {"NAN"} degan qiymat chiqarib beradi
+///// yani {a="1s"} manabu sitring raqam sifatiida ifodalab bo'lmaydi degani
+// let a=2
+// let b="3"
+// console.log(a+ Number(bu  yerga to'g'ridan to'g'ri raqam ham yossak bo'ladi));
+//////  yani : Agar consol.logni icida Number qiymat yozadigan bo'lsak numberni ichidagi qiymatni songa o'zgartirib beradi
+// Kars : sonlarni float sonlar deyiladi
+// console.log(a+ Number(b));
+// console.log(a+ Number.parseInt(b));
+// console.log(a+ parseInt(b));
+// Number-bu faqat sitring bo'lgan sonlarni numberga o'zgartirib berradi
+// parseInt - faqat butun qismini oladi 
+// parseFloat-bu kasr qismini  ham olib beradi 
+//////   Boolean
+// true- ha degani.
+// false- yo'q degani.
+// let a = 1
+// let b = 2
+// console.log(a + b * a);
+// let a = 3
+// let b = 2
+// console.log(a % b);
+// console.log(b % a)
+////////////// '%' -- qoldiqni chiqarish uchun ishlatiladi
+// console.log(2 * 2);  //      javobi: 8
+// console.log(2 * 2 * 2); //   javobi: 8
+// console.log(2 * 2 * 2 * 2);//javobi: 16
+// console.log(2**20); // javobi 1048576 bu 2ta {**} kvadirat maosini  bildiradi va kvadiratga ko'tarib beradi
+// let a = 3
+// let b = 2
+// // a = b
+// console.log(a == b);
+// (" = ")-a qiymatini b qiymatiga tenglab beradi yani b- ning qiymatini a ga beradi 
+// (' == ')- 2ta tenglik qiymatni bir biriga taqqoslash uchun ishlatiladi
+// (' ==== ') - 3 talik qo'shtirnoq ularning typni ham tekshiiradi
+// let a = 1
+// let b = 2
+// let c = 3
+// c = a + c
+// c += a
+// c -= a
+// consol\e.log(c);
+// let a = 1
+// a -= ++a
+// console.log(a);
+/////    +  1ta pilus sonni bit biriga qo'shadi
+////    ++  2ta pilus o'sha songa yana 1 ni qo'shadi
+//      -  1ta pilus sonni bit biriga ayiradi
+////    --  2ta pilus o'sha songa yana 1 ni ayiradi
+// let a = 3
+// b = 2
+// console.log(a >=! b);
+// console.log(a <=! b);
+// console.log(a !== b);
+// <, >, ==, ------taqqoslash belgilari 
+// ! ------ bu undoov belgisi inkor manosida keladi
+//                            console.log(!true);
+// let admin = false
+// let ceo = true
+//                       console.log(admin || ceo);
+// let login = true;
+// let pw = true;
+//                   console.log(login && pw);
+
+
+
+//                                      Homework
+// let
+//     a = 1
+// b = 1
+// c = ++a
+// d = b++
+// console.log(a, b);//javobi:2 2
+// let a = 2
+// let x = 1 + (a *= 2)
+// console.log(x);// javobi:5
+// let a = "3"
+// b = 2 + 1
+// console.log(b+Number(a));
+// let a = "3"
+// b = 1 + 0
+// console.log(a-Number(b));
+// let 
+// a=true
+// b=false
+// console.log(true+false); //  Javobi:1
+// let 
+// a=6
+// b="3"
+// console.log(a/Number(b)); javobi: 2
+// let 
+// a="6"
+// b="3"
+// console.log(a*b); javobi:18
+// let 
+// a=6+5+"px"
+// console.log(a);Javobi:11px
+// let 
+// a="&"+6+5
+// console.log(a);Javobi:&65
+// let  
+// a="4"-2
+// console.log(a);javobi:2
+// let 
+// a="4px"-2
+// console.log(a);javobi:Nan
+// let 
+// a="-9"-5
+// console.log(a);javobi:-14
+// let 
+// a="-9"-5
+// console.log(a);javobi:-95
+// let 
+// a=null+1
+// console.log(a);Javobi:1
+// let 
+// a=undefined+1
+// console.log(a);Javobi:NaN
+// let
+//     a = "\t n\"+1
+// console.log(a); 
+// let 
+// a=5
+// b=4
+// console.log(a>b);Javobi:true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
