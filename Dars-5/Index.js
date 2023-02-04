b2b1143b9f87a85ded842f736defcdcf83c379e1
Muhammadjonmurodullayev <Muhammadjonmurodullayev2005@gmail.com>
2023-02-04 12:55:54 +0500

    Dars-5

--- Dars-5/Index.js
+++ Dars-5/Index.js
@@ -2,11 +2,30 @@
 //     console.log('Hello js');
 // }
 // get()
-let name ="webbrain"
-console.log(name);
-
-
-
+// let name ="webbrain"
+// function get(){
+//     console.log('1');
+// }
+// console.log(2);
+// get()
+// let name ="webbrain"
+// function get(){
+//     let name ="IT"
+//     console.log(name);
+// }
+// // get()
+//  function get(name){
+//     console.log(name);
+//  }
+// get("Muhammadjon")
+let total = 208;
+function get(grade) {
+    let res = (grade * 100) / 208;
+    console.log(res);
+}
+get()
+get(50);
+get(104);
 
 
 
