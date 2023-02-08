7bf1f6d0bee3363db38aadd4e9d2cd3a9dd027e4
Muhammadjonmurodullayev <Muhammadjonmurodullayev2005@gmail.com>
2023-02-08 13:51:36 +0500

    Dars-6

---
+++ Dars-6/index.js
@@ -0,0 +1,4 @@
+ // Objectlar                                               
+   let text ={name:"Muhammadjon",surname:"Murodullayev", year:2005
+}          
+console.log(text);                                   
