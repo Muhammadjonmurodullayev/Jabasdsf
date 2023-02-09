fa460236cf50af9358bb3312dfeeba7c39857788
Muhammadjonmurodullayev <Muhammadjonmurodullayev2005@gmail.com>
2023-02-09 15:10:16 +0500

    Dars-6

--- Dars-6/index.js
+++ Dars-6/index.js
@@ -23,18 +23,26 @@
 // ikkita bo'g'indan iborat so'zlarni chaqirishimiz uchun "" ikkitalik qo'shtirnoqdan foydalanamiz
 // biz raqamlar ham yozishimiz mumkin ularni chaqirganimizda ["1651651"] manashunday holatda chaqiramiz 
 // yangi malumot qo'shish ucun masalan:"account.name = 2020;" Manashunaqa qilib qoshiladi 
+// Object.freeze(account)----bu kodimiz malumotlarni o'zgartira olmasligimiz ucun ishlatiladi
 //   console.log(account.major);
 // console.log(account[key]);
 // console.log(account["key"]);
-let account = {
-  name: "Webbrain Academiy",
-  major: "frontend",
-  foundet: "2020",
-  "full name": "Webbrain IT academiy",
-  }
-account.year =2019
-console.log(account); 
-Object.freeze(account)----bu kodimiz malumotlarni o'zgartira olmasligimiz ucun ishlatiladi
+// Object.freeze(account)
+// Object.seal(account)
+// freeze va seal ni farqi freezeda hechnima o'zgartirib bolmaydi,Sealda esa faqat bor malumoti yangilasa bo'ladi 
+// Object.assign(obj1,obj2) -----yani bu  assign degani obj1ga obj2nii malumotini biriktirdik degani 
+// let account = {
+//   name: "Webbrain Academiy",
+//   major: "frontend",
+//   foundet: "2020",
+//   "full name": "Webbrain IT academiy",
+// }
+// account.year = 2019
+// console.log(account);
+
+
+
+
 
 
 
