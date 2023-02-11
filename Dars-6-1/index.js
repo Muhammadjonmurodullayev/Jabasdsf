4ffca2d34854cd986a83df60ef2ee1a805da7513
Muhammadjonmurodullayev <Muhammadjonmurodullayev2005@gmail.com>
2023-02-11 17:18:34 +0500

    Dars-6

---
+++ Dars-6-1/index.js
@@ -0,0 +1,23 @@
+// let name = { name: 'Muhammadjon' }
+// let surname = { surname: "Murodullayev" }
+// console.log(Object.assign(name, surname));// Yani objet assign ikkita objectni bir biriga qo'shadi.
+// console.log({...name,...surname});// bu siprit opereytori ikkita objektni bir biriga qo'shib  beradi.bu "{...name,...surname}" manashunday yoziladi
+// Objectlar keys va valyudan tashkil topgan.
+// let ins = { 
+//     name:"Muhammadjon",
+//     surname: "Murodullayev", 
+// getname() {
+//     console.log(this.name,this.surname);// this - function ichida objectlarni chaqirishimiz uchun this ishlatiladi u faqat arrfun da ishlamiydi qolgan functionlarda ishlaydi
+// }
+// }
+// ins.getname()
+// let add =Object.entries(ins)//keys va values dan tashkil topgan
+// // console.log(Object.fromEntries(add));//manashu erreydan objektga o'tish
+// let name = "It center ";
+// let ins = { 
+//         name:"Muhammadjon",
+//         surname: "Murodullayev", 
+//     }
+//     const {name:muham} = ins; // yani bu distirakcha deyiladi. biz "." nuqta bilan emas shunchaki keysni o'zini yozib chaqiramiz
+//   console.log(muham);
+  
