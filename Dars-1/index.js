a94a56efc8fa6cfd4e0531457e6d6bae753730ed
Muhammadjonmurodullayev <Muhammadjonmurodullayev2005@gmail.com>
2023-02-17 10:27:18 +0500

    Dars-1

---
+++ Dars-1/index.js
@@ -0,0 +1,15 @@
+// variable
+// var a = 10, b = 10, c = 10;
+// console.log(a+b+c++);
+// console.log(++a);
+// let name = {
+// name:"Muhammadjon",
+// surname:"Murodullayev",
+// year:18
+// }
+// let surname = "surname"
+// console.log(name);
+// console.log(name["surname"]);
+// if (
+//     if  (console.log("Muhammadjon");)
+//     else)
