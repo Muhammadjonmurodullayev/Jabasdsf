056cfebdc39654efac68b8a538ec024b589ea4b4
Muhammadjonmurodullayev <Muhammadjonmurodullayev2005@gmail.com>
2023-02-17 11:59:52 +0500

    Dars-1

--- Dars-1/index.js
+++ Dars-1/index.js
@@ -12,9 +12,22 @@
 // let a = 1, b = 2, c = 3;
 // a=b=c
 // Number
-let num  = 9007199254740992n+1n;
+// let num  = 9007199254740992n+1n;
+// console.log(num);
+// '',"",``,
+// console.log("Hello 'webbraiy academy'");
+// console.log('Hello "webbraiy academy"');
+// console.log(`Hello"webbraiy academy"`);
+// let name  =1.2
+// console.log(typeof name);
+// console.log(true);
+// console.log(false);
+// let a;
+// let b = null;
+// console.log(undefined===null);
+// console.log(typeof undefined,typeof null);
+// console.log( typeof BigInt );
 
-console.log(num);
 
 
 
