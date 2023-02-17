bd82f32e6ea46ee8ed0e4dfb3351c1c1bf00863f
Muhammadjonmurodullayev <Muhammadjonmurodullayev2005@gmail.com>
2023-02-17 17:51:54 +0500

    Dars-1

--- Dars-1/index.js
+++ Dars-1/index.js
@@ -27,11 +27,35 @@
 // console.log(undefined===null);
 // console.log(typeof undefined,typeof null);
 // console.log( typeof BigInt );
+// let a = Symbol("text")
+// let b = Symbol("text")
+// console.log(a==b);
+// // console.log(a===b);
+// let ins = {
+//     name:"Muhammadjon",
+//     surname:"Murodullayev",
+//     pw:231321321,
+//     "full name" :"Murodullayev Muhammadjon"
+// }
+// // console.log(ins["full name"]);
+// console.log(ins["full name"]);
 
-
-
-
-
+// // pirimitiv 
+// Number//
+// String
+// Boolean
+// undefined
+// null
+// BigInt
+// Symbol
+//         // none pirimitiv
+// Object
+// console.log(Number(b));
+// let a=9007199254740992n+1
+// console.log(a);
+// let a = ;
+// let b = false;
+// console.log(a);
 
 
 
