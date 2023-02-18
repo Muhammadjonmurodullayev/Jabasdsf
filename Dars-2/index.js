bb0cbbe44e3bb5862b06f7e80018e04239924f89
Muhammadjonmurodullayev <Muhammadjonmurodullayev2005@gmail.com>
2023-02-18 12:21:18 +0500

    Dars-2

--- Dars-2/index.js
+++ Dars-2/index.js
@@ -4,15 +4,15 @@
 // console.log( typeof a);
 // typof : deb yossak manashu malumotlarni tyupni chiqarib beradi 
 
-//////  +("a") ---- qo'yilsa u aval string edi ketin typni numberga o'zgartirdi
+// ////  +("a") ---- qo'yilsa u aval string edi ketin typni numberga o'zgartirdi
 // let  a=1
 // let b="3s"//misol
-//////    agar stringni ichida har f bo'lib qoldigan bo'sa u {"NAN"} degan qiymat chiqarib beradi
-///// yani {a="1s"} manabu sitring raqam sifatiida ifodalab bo'lmaydi degani
+// ////    agar stringni ichida har f bo'lib qoldigan bo'sa u {"NAN"} degan qiymat chiqarib beradi
+// /// yani {a="1s"} manabu sitring raqam sifatiida ifodalab bo'lmaydi degani
 // let a=2
 // let b="3"
 // console.log(a+ Number(bu  yerga to'g'ridan to'g'ri raqam ham yossak bo'ladi));
-//////  yani : Agar consol.logni icida Number qiymat yozadigan bo'lsak numberni ichidagi qiymatni songa o'zgartirib beradi
+// ////  yani : Agar consol.logni icida Number qiymat yozadigan bo'lsak numberni ichidagi qiymatni songa o'zgartirib beradi
 // Kars : sonlarni float sonlar deyiladi
 // console.log(a+ Number(b));
 // console.log(a+ Number.parseInt(b));
@@ -20,7 +20,7 @@
 // Number-bu faqat sitring bo'lgan sonlarni numberga o'zgartirib berradi
 // parseInt - faqat butun qismini oladi 
 // parseFloat-bu kasr qismini  ham olib beradi 
-//////   Boolean
+// ////   Boolean
 // true- ha degani.
 // false- yo'q degani.
 // let a = 1
@@ -30,7 +30,7 @@
 // let b = 2
 // console.log(a % b);
 // console.log(b % a)
-////////////// '%' -- qoldiqni chiqarish uchun ishlatiladi
+// //////////// '%' -- qoldiqni chiqarish uchun ishlatiladi
 // console.log(2 * 2);  //      javobi: 8
 // console.log(2 * 2 * 2); //   javobi: 8
 // console.log(2 * 2 * 2 * 2);//javobi: 16
@@ -52,10 +52,10 @@
 // let a = 1
 // a -= ++a
 // console.log(a);
-/////    +  1ta pilus sonni bit biriga qo'shadi
-////    ++  2ta pilus o'sha songa yana 1 ni qo'shadi
+// ///    +  1ta pilus sonni bit biriga qo'shadi
+// //    ++  2ta pilus o'sha songa yana 1 ni qo'shadi
 //      -  1ta pilus sonni bit biriga ayiradi
-////    --  2ta pilus o'sha songa yana 1 ni ayiradi
+// //    --  2ta pilus o'sha songa yana 1 ni ayiradi
 // let a = 3
 // b = 2
 // console.log(a >=! b);
@@ -132,29 +132,3 @@
 // a=5
 // b=4
 // console.log(a>b);Javobi:true
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
