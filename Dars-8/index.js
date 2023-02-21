60913a1243696b8693c3039e3263ed1c1d63bbe0
Muhammadjonmurodullayev <Muhammadjonmurodullayev2005@gmail.com>
2023-02-21 15:54:01 +0500

    Dara-8

--- Dars-8/index.js
+++ Dars-8/index.js
@@ -1,3 +1,25 @@
-// let a ="1000000";
-// console.log(Number(a));
-console.log(NaN);
+// let a = 1_000_000_000_000,
+// b = "100_0000"
+// console.log(typeof a ,typeof  b);
+// console.log( a ,b);
+// let a = Math.floor(14.999999999)
+// let a = Math.ceil(14.44444)
+// let a =Math.trunc()Faqat butun qismini olib berradi
+// 10
+// 100
+// // 1000
+// let b = Math.min(100,2,8,75)
+// // let a =Math.max(100,-2,8,75)
+// console.log(b,a);
+// let b= 2**4
+// let a =Math.pow(2,3)
+// console.log(a);
+// let a =Math.sign(-15494840)
+// console.log(a);
+// let a =Math.sqrt(4)
+// let b =Math.cbrt(27)
+// console.log(a,b);
+
+
+
+
