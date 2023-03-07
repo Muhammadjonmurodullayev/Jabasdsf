714422a109564e095206d011331738dbfeba2276
Muhammadjonmurodullayev <Muhammadjonmurodullayev2005@gmail.com>
2023-03-07 21:45:51 +0500

    Dars-5

--- Dars-5/Index.js
+++ Dars-5/Index.js
@@ -1,184 +1,403 @@
-// function get(){
-//     console.log('Hello js');
-//     alert('Hello js');
-// }
-// get()
-// let name ="webbrain"
-// function get(){
-//     console.log('1');
-// }
-// get()
-// console.log(2);
-// get()
-// let name ="webbrain"
-// function get(){
-//     let name ="IT"
-//     console.log(name);
-// }
+// // function get(){
+// //     console.log('Hello js');
+// //     alert('Hello js');
+// // }
 // // get()
-//  function get(name){
-//     console.log(name);
-//  }
-// get("Muhammadjon")
-// let total = 208;
-// function get(grade) {
-//     let res = (grade * 100) / 208;
-//     console.log(res);
-// }
-// get(50);
-// get(104.1);
-// function getname(name,surname,){
-//     console.log(name,surname);
-// }
-// getname("Eshmat","Toshmatov")
-// function get(grade) {
-//     let res = (grade * 100) / 208;
-//     return res;
-// }
-// console.log(get(106) > 50 ? "Passed" : "failed");
-// function get(){} buni xoxlagan joyda chaqira olamiz
-// const get = function(){} -buni faqat o'zidan keyin chaqirish kerak
-// const arrFun=()=>{}
-//                                           Homework
-// for(let i=0;i<3;i++){
-//     console.log(`number ${i}!`);
+// // let name ="webbrain"
+// // function get(){
+// //     console.log('1');
+// // }
+// // get()
+// // console.log(2);
+// // get()
+// // let name ="webbrain"
+// // function get(){
+// //     let name ="IT"
+// //     console.log(name);
+// // }
+// // // get()
+// //  function get(name){
+// //     console.log(name);
+// //  }
+// // get("Muhammadjon")
+// // let total = 208;
+// // function get(grade) {
+// //     let res = (grade * 100) / 208;
+// //     console.log(res);
+// // }
+// // get(50);
+// // get(104.1);
+// // function getname(name,surname,){
+// //     console.log(name,surname);
+// // }
+// // getname("Eshmat","Toshmatov")
+// // function get(grade) {
+// //     let res = (grade * 100) / 208;
+// //     return res;
+// // }
+// // console.log(get(106) > 50 ? "Passed" : "failed");
+// // function get(){} buni xoxlagan joyda chaqira olamiz
+// // const get = function(){} -buni faqat o'zidan keyin chaqirish kerak
+// // const arrFun=()=>{}
+// //                                           Homework
+// // for(let i=0;i<3;i++){
+// //     console.log(`number ${i}!`);
 
-// }
-// javobi:
-// number 0!
-// number 1!
-// number 2!
-// let age =19;
-// function checkAge(age){
-//     console.log(age);
-//     if(age>18){
-//         return true;
-//     }else{
-//         console.log("Siz akaunt ocholmaysiz");
-//         return confirm('Did parents allow you?')
-//     }
-// }
-// checkAge(age)
-// console.log(Min(2,5));
-// Pow(3, 2) = 3 * 3=9
-// Pow(3, 3) = 3 * 3 * 3=27
-// Pow(1,100) = 1*1...*1=1
-// console.log(3**2);=9
-// console.log(3**3);=27
-// console.log(1*100);=100
+// // }
+// // javobi:
+// // number 0!
+// // number 1!
+// // number 2!
+// // let age =19;
+// // function checkAge(age){
+// //     console.log(age);
+// //     if(age>18){
+// //         return true;
+// //     }else{
+// //         console.log("Siz akaunt ocholmaysiz");
+// //         return confirm('Did parents allow you?')
+// //     }
+// // }
+// // checkAge(age)
+// // console.log(Min(2,5));
+// // Pow(3, 2) = 3 * 3=9
+// // Pow(3, 3) = 3 * 3 * 3=27
+// // Pow(1,100) = 1*1...*1=1
+// // console.log(3**2);=9
+// // console.log(3**3);=27
+// // console.log(1*100);=100
 
-// return - returnga qanaqa qiymat biriktirilsa funksiyaninig qiymat o'sha bo'lib qoladi
-// function getname(){}-------------Function declerishin
-// const getname = function(){}-----funnction expreshin
-// arrFun =()=>{}-------------arrFun deyiladi
-
-
-
-// let ball =200;
-// function get(foiz){
-//  let res =(foiz *100)/200;
-//     console.log(res);
-// }
-// get()
-// get(60)
-// get(70)
-
-// function getname(name){
-//     return name;
-// }
-// console.log(getname("Muhammadjon"));
-
-// let ball = 200;
-// function get(foiz) {
-//     let res = (foiz * 100) / 200;
-//     return res;
-// }
-// console.log(get(200) > 50 ? "Passed" : "failde");
-// function getname(){}fun dec
-// const getGrade =function(){} fuc exp
-// const arrFun =()=>{} arrfun
-//    Homework
-// for (let i =0; i<3; i++){
-//     console.log(`number ${i}!`);
-// }
-// javobi:
-// number 0!
-// number 1!
-// number 2!
-
-// function name(yosh){
-//     if (yosh>18 ){
-//         console.log("Ok");
-//     }else{
-//         console.log('Akoun ocholmaysiz');
-//     }
-// }
-// name(19)
-// function name (yosh){
-//     yosh>18 ? console.log("ok"): console.log('Akoun ocholmaysiz');
-
-// }
-// name(19)
-// function name (son,number){
-//     if (son<number){
-//         console.log(son);
-//     }else{
-//         console.log(number);
-//     }
-// }
-// name(2,5)
-// function name (son,number){
-//         if (son<number){
-//             return son;
-//         }else{
-//             return number;
-//         }
-//     }
-//   console.log(name(2,5));
-// function  name(son,number){
-//     if (son<number){
-//         console.log(son);
-//     }else{
-//         console.log(number);
-//     }
-// }
-//     // name(3,-1)
-//     function name(son,number){
-//         if (son<number){
-//             console.log(son);
-//         }else{
-//             console.log(number);
-//         }
-//     }
-// name(1,1)
+// // return - returnga qanaqa qiymat biriktirilsa funksiyaninig qiymat o'sha bo'lib qoladi
+// // function getname(){}-------------Function declerishin
+// // const getname = function(){}-----funnction expreshin
+// // arrFun =()=>{}-------------arrFun deyiladi
 
 
-// function pow(a, b) {
-//     let sum = 1;
-//     for (let i = 0; i < b; i++) {
-//         sum=sum*a
-//         }console.log(sum);
-// }
-// pow(2,5)
-// pow(3,3
-// pow(1,100)
-// // console.log(2**2);
-// () => {}
 
-//  ask = (question, yes, no) => {
-//     if (confirm(question)) yes();
-//     else no()
-// }
+// // let ball =200;
+// // function get(foiz){
+// //  let res =(foiz *100)/200;
+// //     console.log(res);
+// // }
+// // get()
+// // get(60)
+// // get(70)
 
-// ask(
-//     "Do you agree?",
-//     ()=> { alert("You agreed"); },
-//      ()=> { alert("You canceld the execution"); }
-// )
-// confirm()
-// alert("pafaf")
-// prompt("f,gpxd,h")
-// console.log(a);
-// const a = 1;
+// // function getname(name){
+// //     return name;
+// // }
+// // console.log(getname("Muhammadjon"));
+
+// // let ball = 200;
+// // function get(foiz) {
+// //     let res = (foiz * 100) / 200;
+// //     return res;
+// // }
+// // console.log(get(200) > 50 ? "Passed" : "failde");
+// // function getname(){}fun dec
+// // const getGrade =function(){} fuc exp
+// // const arrFun =()=>{} arrfun
+// //    Homework
+// // for (let i =0; i<3; i++){
+// //     console.log(`number ${i}!`);
+// // }
+// // javobi:
+// // number 0!
+// // number 1!
+// // number 2!
+
+// // function name(yosh){
+// //     if (yosh>18 ){
+// //         console.log("Ok");
+// //     }else{
+// //         console.log('Akoun ocholmaysiz');
+// //     }
+// // }
+// // name(19)
+// // function name (yosh){
+// //     yosh>18 ? console.log("ok"): console.log('Akoun ocholmaysiz');
+
+// // }
+// // name(19)
+// // function name (son,number){
+// //     if (son<number){
+// //         console.log(son);
+// //     }else{
+// //         console.log(number);
+// //     }
+// // }
+// // name(2,5)
+// // function name (son,number){
+// //         if (son<number){
+// //             return son;
+// //         }else{
+// //             return number;
+// //         }
+// //     }
+// //   console.log(name(2,5));
+// // function  name(son,number){
+// //     if (son<number){
+// //         console.log(son);
+// //     }else{
+// //         console.log(number);
+// //     }
+// // }
+// //     // name(3,-1)
+// //     function name(son,number){
+// //         if (son<number){
+// //             console.log(son);
+// //         }else{
+// //             console.log(number);
+// //         }
+// //     }
+// // name(1,1)
+
+
+// // function pow(a, b) {
+// //     let sum = 1;
+// //     for (let i = 0; i < b; i++) {
+// //         sum=sum*a
+// //         }console.log(sum);
+// // }
+// // pow(2,5)
+// // pow(3,3
+// // pow(1,100)
+// // // console.log(2**2);
+// // () => {}
+
+// //  ask = (question, yes, no) => {
+// //     if (confirm(question)) yes();
+// //     else no()
+// // }
+
+// // ask(
+// //     "Do you agree?",
+// //     ()=> { alert("You agreed"); },
+// //      ()=> { alert("You canceld the execution"); }
+// // )
+// // confirm()
+// // alert("pafaf")
+// // prompt("f,gpxd,h")
+// // console.log(a);
+// // const a = 1;
+// // function name(name,surname){
+// // return "gfgf"
+
+// // }
+// // console.log(name());
+// // const name = function(){}// declaration
+// // function name(){}//expreshhin
+// // name(()=>{})
+// // const name = ()=>{}//arrow funtion 
+//                        // Homework
+// // function checkage(age){
+// //     if(age>18){
+// //         return true;
+// //     }else{
+// //         return confirm ('Did parents')
+// //     }
+// // }
+// // function get(age){
+// //     age>=18?console.log(`Fezbuk akount ocha olasiz`):console.log(`Fezbuk akount ocha olmaysiz`);
+// // }
+// // get(18)
+// // function get(son1,son2){
+// //  console.log(Math.min(son1,son2));
+
+// // }
+// // get(12,23)
+// // function pow(pow1,pow2){
+// //     console.log(Math.pow(pow1,pow2));
+// // }
+// // pow(5,2)
+// // pow(3,3)
+// // pow(100,1)
+// // function ask(questions,yes,no){
+// //     if(confirim(question))yes();
+// //     else no()
+// //     ask(
+// //         "Do you agree",
+// //         function(){alert("You agred.");}
+// //         function(){alert("You canceled the execution.");}
+
+// //     )
+// // }
+// // let  a=12
+// // // console.log(a);
+// // function get(age="Bunaqa  malumot mavjud emas"){{
+// //     console.log(age);
+// // }}
+
+// // get("Hello js")
+// // function name(){}///funtion expretion
+// // const name = function (){}//funton declerition
+// // const name = ()=>{}// arr funtion
+// function name(){
+
+// }
+// name(()=>{})
+// //  // Objectlar                                               
+// //    let text ={name:"Muhammadjon",surname:"Murodullayev", year:2005
+// // }          
+// // console.log(text);          
+// //  kiy: valyu------deyiladi 
+// // kiy,name,indeketor-yani ko'rsatuvchi:yalyu         
+// // let account = {
+// // name:"Webbrain Academiy",
+// // major:"frontend",
+// // foundet:"2020"
+// // }`
+// // console.log(account);
+// // let obj = new Object();
+// // console.log(obj);
+// ////////// Object ichidagi har bittasini olib kelish uchun ". nuqta" nuqtadan foydalanamiz
+// // let account = {
+// //   name:"Webbrain Academiy",
+// //   major:"frontend",
+// //   foundet:"2020",
+// //   "full name" : "Webbrain IT academiy"
+// // }
+// // console.log(account["full name"]);
+// // ikkita bo'g'indan iborat so'zlarni chaqirishimiz uchun "" ikkitalik qo'shtirnoqdan foydalanamiz
+// // biz raqamlar ham yozishimiz mumkin ularni chaqirganimizda ["1651651"] manashunday holatda chaqiramiz 
+// // yangi malumot qo'shish ucun masalan:"account.name = 2020;" Manashunaqa qilib qoshiladi 
+// // Object.freeze(account)----bu kodimiz malumotlarni o'zgartira olmasligimiz ucun ishlatiladi
+// //   console.log(account.major);
+// // console.log(account[key]);
+// // console.log(account["key"]);
+// // Object.freeze(account)
+// // Object.seal(account)
+// // freeze va seal ni farqi freezeda hechnima o'zgartirib bolmaydi,Sealda esa faqat bor malumoti yangilasa bo'ladi 
+// // Object.assign(obj1,obj2) -----yani bu  assign degani obj1ga obj2nii malumotini biriktirdik degani 
+// // let account = {
+// //   name: "Webbrain Academiy",
+// //   major: "frontend",
+// //   foundet: "2020",
+// //   "full name": "Webbrain IT academiy",
+// // }
+// // account.year = 2019
+// // console.log(account);
+// // let account ={
+// //   name:"Muhammadjon",
+// //   surname:"Murodullayev",
+// //   year:2005,
+// //   "full name" :"Muhammadjon Muroullayev"
+// // }
+// // let key = "year";
+// // console.log(account);
+// // let obj = new Object();
+// // console.log(obj);
+// // console.log(account.name);
+// // console.log(account[key]);
+// // console.log(account["full name"]);
+// // console.log(account[0]);
+// // Object.freeze(account)
+// // Object.seal(account)
+// // account.fullname = "Webbrain academy"
+// // account.name = "Eshmat"
+// // console.log(account);
+// // const key = "surname";
+
+// // let account ={
+// // name:"Muhammadjon",
+// // surname:"Murodullayev",
+// // year:2005
+
+// // }
+// // for ( let i in account)
+// // console.log(i);
+
+// // let user = {
+// //     name: "John",
+// //     surname: "Smith",
+
+// // }
+// // user.name = "Pete";
+// // delete user.name;
+// // console.log();
+// // let schdule = {}
+// // alert(isEmpty(schudele));
+// // schudele["8:30"]="Get up";
+// // alert(isEmpty(schdule));
+
+// // for (key in schdule) {
+// //     console.log();
+// //     if (key) {
+// //         console.log(false);
+// //     }
+
+// // }
+// // console.log(schdule);
+
+// // function check(obj) {
+// //     for (key in obj) {
+// //         if (typeof key === "undefined");
+// //         return console.log(true);
+// //     }
+// // }
+// // console.log(check(schdule));
+// // let ish = {
+// //     John: 100,
+// //     Pet: 160,
+// //     Ann: 130,
+
+// // }
+// // let sum = 0;
+// // for (value in ish ) {
+// //     if (ish[value] > sum){
+// //         sum=ish[value]
+// //     }
+// // }
+// // // console.log(sum);
+// // let ish = {
+// //     John: 200,
+// //     Pet: 300,
+// //     title: "My menu"
+// // }
+// // let sum = 0;
+// // for (value in ish) {
+// //     let res = ish[value] *2
+// //     console.log(res);
+// //     // if (ish[value] > sum) {
+// //     //     console.log(ish[value] * 2);
+//     // }
+// // }
+// // for ( value in ish){
+// //     // console.log(ish[value]);
+// //     if  (typeof ish[value]=="number"  ){
+// //         console.log(ish[value]*2);
+// //     }
+// // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
