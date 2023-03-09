c3c12fcf070031767ce77e4dee36a16ef13d5cc3
Muhammadjonmurodullayev <Muhammadjonmurodullayev2005@gmail.com>
2023-03-10 00:15:29 +0500

    Dars-3

--- Dars-3/index.js
+++ Dars-3/index.js
@@ -152,9 +152,62 @@
 
 // let message
 // login == "Employee" ? message = "hello" : login == "Director" ? message = "Greetings" : login == "" ? message = "NO login" : message ="";
+//    If else
+// =====================================================================================================================
+//                               1)Misol
+// let login = 12
+// let pw = 12
+// if(login&&pw){
+//     console.log('Rigth!');
+// }else{
+//     console.log('You dont know "Ecmascript"');
+// }
+// // =====================================================================================================================
+// ========================================================================================================================
+//                               2)Misol
+// let son = 0
+// if(son==0){
+//     console.log(0);
+// }
+// else if  (son>1){
+// console.log(1);
+// }else{
+// console.log(-1);
+// }
+// =========================================================================================================================
+//                                3)Misol
+// let a = 1, b = 2
+// if (a + b < 4) {
+//     console.log('Below');
+// } else {
+//     console.log('Over');
+// }
+// a+b<4?console.log('Below'):console.log('Over');//javobi
+// ============================================================================================================================
+                                   // 4)Misol
+// let login='director'
+// if(login=='eployee'){
+//     console.log('hello');
+// }else if (login=='director'){
+//     console.log('Greetings');
+// }else if(login==""){
+//     console.log('no login');
+// }else{
+//     console.log('');
+// }
+// login=='eployee'?console.log('Hello'):login=='director'?console.log('Greetings'):login==""?console.log('no login'):console.log("");//javobi
+// ========================================================================================================================================================
+//                                                         switch
+//                                                         5)Misol
+// let browser ='Opera'
+// switch(browser){
+//     case 'Edge':console.log('You got the Edge!');break;
+//     case 'Chrome' :
+//     case 'Firefox':
+//     case 'Safari' :
+//     case 'Opera'  :console.log('Okey we support these browsers too');break;
+//     default:console.log('we hope that this page looks ok!');
 
-
-
-
-
+// }
+// ==============================================================================================================================
 
