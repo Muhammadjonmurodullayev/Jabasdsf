5f98f5b66cc044641d6683f075e588b14e74807c
Muhammadjonmurodullayev <Muhammadjonmurodullayev2005@gmail.com>
2023-03-13 19:47:14 +0500

    Dars-8

--- Dars-8/index.js
+++ Dars-8/index.js
@@ -31,6 +31,104 @@
 // console.log(a.toFixed(10));
 // let a = 12.37+"as"
 // console.log(isNaN(a));
+// console.log();
+// Homework
+// let a = 
+// a = a.toString(2)
 
+// for (a = 0; a < a.length; a++) {
+//     var nol = 0  
+//     var bir = 0
+//     if (i[i] === 0) {
+//         nol++
+//     } else if (i[i]) {
+//         bir++
+//     }
+// }
+// console.log(nol, bir);
 
-
+// console.log(a);
+// let a =Math.trunc(Math.random()*50)
+// console.log(a);
+// // console.log(obj);
+// info ={
+//     info:4545
+// }
+// // console.log(Object.assign(obj,info));
+// // console.log(obj.assign);
+// obj.age()
+// let obj ={
+//     name:"Muhammadjon",
+//     surname:"Murodullayev",
+//     age:1515
+// }
+// let info ={
+//     box:"box1"
+// }
+// console.log(...obj,...info);
+// // console.log(Object.entries(obj))
+// let num = 10_00_000///osonroq     bo'lishi uchuun
+// let num = 10e5////////// osonroq     bo'lishi uchuun
+// let a = `12.1a2`
+// console.log(Number(a));//-----------oz xolicha numberga o'zgartirib beradi 
+// console.log(parseInt(a));//---u faqar sonni butun qismini oolib beradi 
+// console.log(Number.parseFloat(a));//bu ham sonni butun qismini ham kasir qismini ham olib beradi bu faqat biriorta harf ko'rdimi o'shayerda toxtaydide oshangacha bo'lgan malumotlarni olib beradi
+// console.log(Number(a));
+// console.log(parseInt(a));
+// console.log(parseFloat(a));
+// console.log(Number.parseFloat(a));
+// a***
+// console.log(a ** 2 * (b));
+// console.log(b ** a);
+// console.log(b+b ** a);
+// let a = 2
+// let b = 3.65
+// console.log(Math.sqrt(25)+Math.cbrt(27))
+// // //                                         Math-----Matematikadagi arifmetok amallarni bajarishda yordam beradi.
+// console.log(Math.abs(a));//Absolyut qiymat faqat (+) pilus qiymat qabull qiladi 
+// console.log(Math.round(b)); round yaxlidlash uchun ishlatiladi
+// console.log(Math.ceil(5.0001));/// bu doim kasr qismi bo'lsa doim butun qismi nima bo'lishidan qatiy nazar unga butun qismiga birni qo'shib beradi
+// console.log(Math.floor(3.999));// faqar sonni butun qismini chiqarib beradi 
+// console.log(Math.trunc(12.586));// Bu qiymat faqar butun qismini olib beradi. Buni parseInt ga taqoslasak bo'ladi 
+// console.log(Math.trunc(-12.586));// Bu qiymat faqar butun qismini olib beradi. Buni parseInt ga taqoslasak bo'ladi 
+// 10,100,1000,10000/=========rendom
+// console.log(Math.floor(Math.random()*10)); Bizga xoxlagan sonlarni chiqarib bberadi.Ko'aytirishdan keyin nechi son yozsak o'shani ichidagi sonllarni chiqarib beradi
+// console.log(Math.min(58,69,-75,76,));// Bu bizga faqat sonlar ichidan eng kichigini chiqarib beradi 
+// console.log(Math.max(58,69,-75,76,));// Bu bizga faqat sonlar ichidan eng kottasini chiqarib beradi
+// // console.log(Math.pow(-2,2));// Pow Bizaga darajaga Ko'tarib beradi 
+// console.log(Math.sign(500));// ---------  1 son( 1 )dan kotta bolsa ( 1 ) qaytaradi 
+// console.log(Math.sign(0));///  ---------  0 son ( 0 ) teng bolsa ( 0 )    qaytaradi 
+// console.log(Math.sign(-500));//--------- -1 son ( -1 ) past bolsa ( -1 )  qaytaradi
+// console.log(Math.sqrt(25));// BU bizga ildiz osti degani yani bizga ildiz ostidan chiqarb beradi
+// console.log(Math.cbrt(27));// Bu bizga kub ildiz osti degani yani bizga kub ildiz ostidan chiqarb beradi
+// console.log(Math.trunc(25**(1/2)));
+// console.log(Math.sin(1/2));
+// console.log(Math.cos(12));
+// let a= 3
+// console.log(a.toString(2));// 2 lik sanoq sistemasiga o'qtqazib beradi 
+// let n = 2.895
+// console.log(n.toFixed(2));///// bu kasirdan keyin ikita raqmni olib ber deydi 
+// console.log(n.toPrecision(2));//bu hammasaini chidan faqat ikita raqam olib ber deydi
+// console.log(isNaN(`a`));// Bu qiymat raqammi deb so'raydi 
+// console.log(parseInt(`111`,2));
+// let n = 10
+// let nol = 0
+// let bir = 1
+// console.log(n.toString(2));
+// for (i in n) {
+//     if (i == 0) {
+//         nol=nol + 1
+//     console.log(i);
+//     }else if (i == 1){
+//         bir=bir+1
+//     }
+// }
+// console.log(nol,bir);
+// let name = Math.abs(26-258)
+// console.log(name);
+console.log(let name =Math.abs());
+function get(name){
+let name = Math.abs(26-258)
+console.log(name);
+    }
+    get()
