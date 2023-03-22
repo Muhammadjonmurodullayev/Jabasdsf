29ccef38286d9b216a4bc4efe22f900391b0d7e5
Muhammadjonmurodullayev <Muhammadjonmurodullayev2005@gmail.com>
2023-03-22 14:41:49 +0500

    Dars-10

---
+++ Dars-10/index.js
@@ -0,0 +1,70 @@
+// function get(str){
+//     let b = ""
+//     for (i = str.length-1; i > 0; i--) {
+//         b += str[i]
+//     }
+//     console.log(b);
+// }
+// get("Muhammadjon")
+// let str = "web"
+// let res= ""
+// for(i of str){
+//     res+=i+i
+// }
+// console.log(res);
+// let a = "muhammadjon"
+// let res = ""
+// for (i =a.length-1; i > 0; i--) {
+//     res=res+a[i]
+// }
+// console.log(res);
+// let a= "123456789"
+// res = ""
+// for(i of a){
+// res+=i+i
+// }
+// console.log(res);
+
+// let a = {
+//     name: "Muhammadjon",
+//     age: 2,
+//     num: 3
+// }
+// for (i in a) {
+//     if (typeof a[i] == "number") {
+//         a[i] *= 2
+//     }
+
+// }
+// console.log(a);
+//                                         arr // Tarifi----faqat valyudan tashkil topgan malumot turiga aytiladi
+// let arr =[]     //// arr ni belgisi
+// let arr1 =[]// buni ichiga xoxlagan data turini kirita olamiz
+// console.log(typeof arr);muh
+// let obj ={
+//     name:"Muhammadjon"
+// }
+//        pirmitoiv
+// Number
+// Boolean
+// null
+// undefined
+// BigInt 
+// String
+// Symbol
+//                none primitiv
+// Object
+// let arr = [2005, "Muhammadjon","Murodullayev",]
+// console.log(arr[1],arr[2]);
+// let arr = "web"
+// let b = ""
+// for(i = arr.length-1;i<0;i--){
+//     b=b+arr[i]
+// }
+// console.log(b);
+// console.log(Muhammadjon);
+
+
+
+
+
