4fddb1949a2f27cd76bb97f5013090346165ccb4
Muhammadjonmurodullayev <Muhammadjonmurodullayev2005@gmail.com>
2023-03-26 11:24:56 +0500

    Dars-11

---
+++ Dars-11/index.js
@@ -0,0 +1,34 @@
+let users = [
+    { id: 1, year: 1998, engine: 1, name: "Tico" },
+    { id: 2, year: 2005, engine: 1.2, name: "Matiz" },
+    { id: 3, year: 2010, engine: 1.5, name: "Gentra" },
+    { id: 4, year: 2010, engine: 1.2, name: "Cobalt" },
+    { id: 5, year: 2012, engine: 2, name: "Malibu" },
+    { id: 6, year: 2000, engine: 1.2, name: "Damaz" },
+    { id: 7, year: 2018, engine: 2.4, name: "Tracer" },
+]
+// =====================================================================================================================================================
+// ,                       R - Read Malumotni ekranga chiqarishimiz ( Read == o'qish degani ) hisoblanadi 
+// const getyear = () => {
+//     let res = users.sort((a,b) => a.year - b.year)
+//     users=res
+// }
+// getyear()
+// console.log(users);
+// =====================================================================================================================================================
+//                         R - Read Malumotni ekranga chiqarishimiz ( Read == o'qish degani ) hisoblanadi 
+// const getAlfabit=()=>{
+//     let res=users.sort((a,b)=>a.name.localeCompare(b.name))
+//     users=res
+// }
+// getAlfabit()
+// console.log(users);
+// =====================================================================================================================================================
+//                                                    Delete
+// const getdelete=(id)=>{
+// let res =users.filter(valyus => valyus.id !== id)
+// users=res
+// }
+// getdelete()
+// console.log(users);
+// =====================================================================================================================================================
