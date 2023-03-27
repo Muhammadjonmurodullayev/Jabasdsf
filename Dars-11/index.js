59808e71f5f29ecb117ccef93d845e0594e492c3
Muhammadjonmurodullayev <Muhammadjonmurodullayev2005@gmail.com>
2023-03-27 12:01:18 +0500

    Dars-11

--- Dars-11/index.js
+++ Dars-11/index.js
@@ -1,13 +1,13 @@
-let users = [
-    { id: 1, year: 1998, engine: 1, name: "Tico" },
-    { id: 2, year: 2005, engine: 1.2, name: "Matiz" },
-    { id: 3, year: 2010, engine: 1.5, name: "Gentra" },
-    { id: 4, year: 2010, engine: 1.2, name: "Cobalt" },
-    { id: 5, year: 2012, engine: 2, name: "Malibu" },
-    { id: 6, year: 2000, engine: 1.2, name: "Damaz" },
-    { id: 7, year: 2018, engine: 2.4, name: "Tracer" },
-]
-// =====================================================================================================================================================
+// let users = [
+//     { id: 1, year: 1998, engine: 1, name: "Tico" },
+//     { id: 2, year: 2005, engine: 1.2, name: "Matiz" },
+//     { id: 3, year: 2010, engine: 1.5, name: "Gentra" },
+//     { id: 4, year: 2010, engine: 1.2, name: "Cobalt" },
+//     { id: 5, year: 2012, engine: 2, name: "Malibu" },
+//     { id: 6, year: 2000, engine: 1.2, name: "Damaz" },
+//     { id: 7, year: 2018, engine: 2.4, name: "Tracer" },
+// ]
+// // =====================================================================================================================================================
 // ,                       R - Read Malumotni ekranga chiqarishimiz ( Read == o'qish degani ) hisoblanadi 
 // const getyear = () => {
 //     let res = users.sort((a,b) => a.year - b.year)
@@ -32,3 +32,31 @@ let users = [
 // getdelete()
 // console.log(users);
 // =====================================================================================================================================================
+//           Ichida nechta katta harf bor yoki yo'q ligini tekshirib beradi  
+// let arr = "WeBBrain aCCademiy"
+// let res = 0
+// let res1 = 0
+// for (i of arr) {
+//     if (i === i.toUpperCase() && i !== " ") {
+//         res++
+//     }
+//     else if(i === i.toLowerCase()&& i !== " "){
+//         res1++
+//     }
+// }
+// console.log(`${res}-ta kata harf 
+// ${res1}-ta kichkina harf`);
+// ====================================================================================================================================
+// stringda berilgan ("@") su belgini chiqarib yuborish
+let str = "Webb@in Ac@demiy"
+let res= ""
+for (i of str) {
+    if(i == "@"){
+        res+=i="a"
+    }else{
+        res+=i
+    }
+    str=res
+}
+console.log(str);
+// ====================================================================================================================================
